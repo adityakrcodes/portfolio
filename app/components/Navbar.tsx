b51a1dae323798018157e1bf38ebb40ecdaf6701
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2025-04-03 23:49:47 +0530

    Add Navbar component and integrate it into the homepage

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-800 text-white flex justify-between items-center px-10 py-2">
+            <div className="flex justify-between items-center">
+            {/* logo */}
+                <div className="p-2 bg-gray-900 rounded-lg flex items-center">
+                    AKC
+                </div>
+            </div>
+            <div className='flex items-center space-x-4'>
+                {/* Links */}
+                <Link href="/" className='p-2 underline underline-offset-3'>Home</Link>
+                <Link href="/about" className='p-2'>About</Link>
+                <Link href="/services" className='p-2'>Projects</Link>
+                <Link href="/blog" className='p-2'>Blog</Link>
+                <Link href="/contact" className='p-2'>Contact</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center p-5">
-        <h1 className="text-3xl font-bold">Namaste 🙏🏻</h1>
-        <p className="text-xl mt-5 font-bold text-center">
-            I am Aditya Kumar a Full Stack Web Developer working with MERN and Solana Blockchain.
-        </p>
+    <div>
+        <Navbar />
     </div>
   );
 }
