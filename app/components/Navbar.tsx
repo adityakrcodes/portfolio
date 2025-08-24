34686374b59ec9c57815a38be17b38296d509ec5
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2025-08-24 12:15:11 +0530

    add: added pnpm-lock.yaml

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,17 +6,17 @@ const Navbar = () => {
         <nav className={`bg-gray-800 text-white flex justify-between items-center px-10 py-2 ${logo.className}`}>
             <div className="flex justify-between items-center">
             {/* logo */}
-                <div className="p-2 bg-gray-900 rounded-lg flex items-center">
+                <div className="p-3.5 bg-gray-900 rounded-lg flex items-center text-2xl border border-gray-700">
                     AKC
                 </div>
             </div>
-            <div className='flex items-center space-x-4'>
+            <div className='flex items-center space-x-4 text-2xl'>
                 {/* Links */}
-                <Link href="/" className='p-2 underline underline-offset-3'>Home</Link>
-                <Link href="/about" className='p-2'>About</Link>
-                <Link href="/services" className='p-2'>Projects</Link>
-                <Link href="/blog" className='p-2'>Blog</Link>
-                <Link href="/contact" className='p-2'>Contact</Link>
+                <Link href="/" className='p-2 underline underline-offset-3'>home</Link>
+                <Link href="/about" className='p-2'>about</Link>
+                <Link href="/services" className='p-2'>projects</Link>
+                <Link href="/blog" className='p-2'>blog</Link>
+                <Link href="/contact" className='p-2'>contact</Link>
             </div>
         </nav>
     );
