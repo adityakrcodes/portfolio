f25a6d4188966e30b9f2134152254d5ea64fcd53
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2025-10-19 20:13:07 +0530

    style: refine Home page layout for better width and alignment of elements

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { logo, text } from "./styles/fonts";
 export default function Home() {
   return (
     <div className="flex flex-col h-full">
-        <div className="flex-1 flex md:flex-row flex-col-reverse justify-center items-center gap-10 md:px-10 min-h-[80vh w-10/12 mx-auto">
+        <div className="flex-1 flex md:flex-row flex-col-reverse justify-center items-center gap-10 md:px-10 w-7/12 mx-auto">
             <div className="md:w-5/12 w-10/12 text-xl md:text-2xl">
                 <h1 className={`${logo.className} text-4xl md:text-6xl`}>Yo 👋🏻</h1>
                 <p className={`${logo.className} text-4xl md:text-5xl`}>I am Aditya</p>
@@ -11,7 +11,7 @@ export default function Home() {
                     I am a MERN Stack Developer and I love to build websites and web applications.
                 </p>
             </div>
-            <div className="md:w-3/4 w-10/12 flex justify-center items-center">
+            <div className="md:w-3/4 w-12/12 flex justify-center items-center">
                 <img src="/images/pfp.png" alt="profile" className="md:w-7/12 h-full object-cover rounded-full shadow-[0_0_40px_rgba(79,196,255,0.25)]" />
             </div>
         </div>
